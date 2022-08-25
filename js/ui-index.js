87debf24ec1451f7d8bf3e7fecc83ffe89300fe8
Temiloluwa <temiloluwaogunti8@gmail,com>
2022-08-25 03:12:50 +0100

    added selection event

--- js/ui-index.js
+++ js/ui-index.js
@@ -1,41 +1,34 @@
-// document.body.style.backgroundColor = 'blue';
-// document.body.style.color = "red";
-
-// const responseData = require('../Api/dictionary');
-
 import findMeaning from "../Api/dictionary.js";
 console.log(await findMeaning("hello"));
 
-// console.log(responseData)
-
-const apiData = () => {
-  return {
-    hasError: false,
-    errorMessage: null,
-    result: [
-      {
-        word: "gotcha",
-        phonetics: "/djnjcr",
-        audio: "music.mp3",
-        definitions: [
-          {
-            partOfSpeech: "noun",
-            meaning: "returns the name of a word",
-            example: "gotcha is the most used extension in the world",
-            synonyms: ["bright", "david", "temi"],
-            antonyms: ["bad", "nah"],
-          },
-          {
-            otherDefinitions: "nah nah nah",
-          },
-        ],
-      },
-      {
-        otherresults: "Other results under result",
-      },
-    ],
-  };
-};
+// const apiData = () => {
+//   return {
+//     hasError: false,
+//     errorMessage: null,
+//     result: [
+//       {
+//         word: "gotcha",
+//         phonetics: "/djnjcr",
+//         audio: "music.mp3",
+//         definitions: [
+//           {
+//             partOfSpeech: "noun",
+//             meaning: "returns the name of a word",
+//             example: "gotcha is the most used extension in the world",
+//             synonyms: ["bright", "david", "temi"],
+//             antonyms: ["bad", "nah"],
+//           },
+//           {
+//             otherDefinitions: "nah nah nah",
+//           },
+//         ],
+//       },
+//       {
+//         otherresults: "Other results under result",
+//       },
+//     ],
+//   };
+// };
 
 const response = apiData();
 
@@ -48,31 +41,22 @@ const destructuredData = () => {
 
   const syn = synonyms.filter((_, idx)=> idx < 2);
 
-  document.getElementById("main").innerHTML = `
-        <h2>${word}</h2>
-        <span>${phonetics}</span>
-        <button id="btn">sound</button>
-        <p>${meaning}</p>
-        <div>${partOfSpeech}</div>
-        <span>${syn}</span>
-        <span>${antonyms}</span>
-  `;
 };
 destructuredData(response)
-
-// const Btn = document.getElementById("btn");
-
-// Btn.addEventListener('click', ()=>{
-//   console.log('working')
-// })
-
-// console.log(window)
 /*
 for display position, innerHeight and width / 2..
 
 so it'd always pop up at the center of the screen.
 */ 
 
-const closePop_btn = document.querySelector(".pop__close");
+const body = document.querySelector("body");
 
-console.log(closePop_btn);
+body.addEventListener('click', () => {
+  const selectedTxt = handleSelection();
+  console.log(selectedTxt);
+})
+
+function handleSelection() {
+  let selectedTxt = window.getSelection().toString();
+  return selectedTxt
+}
