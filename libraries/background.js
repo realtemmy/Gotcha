9ebf2dc3efc8d626643413203cb901b15fe72dd6
GEORGE EBISIKE <georgeebisike@gmail.com>
2022-09-01 03:32:34 +0100

    more code refactoring for extension

--- libraries/background.js
+++ libraries/background.js
@@ -6,6 +6,15 @@ var userMessage = "";
 function reciever(request, sender, sendResponse) {
     console.log(request, "The request");
     userMessage = request;
+
+    // switch (request.directive) {
+    //     case "sound":
+    //         chrome.tabs.executeScript(null, {file: "content.js", allFrames: true})
+    //         break;
+    
+    //     default:
+    //         break;
+    // }
 }
 
 
