40c6bf5e78bdb14de015f2322b56f7744be6e09a
GEORGE EBISIKE <georgeebisike@gmail.com>
2022-09-01 09:43:48 +0100

    v1 ready

--- libraries/content.js
+++ libraries/content.js
@@ -1,23 +1,15 @@
-const mediumHighlighter = document.createElement("medium-highlighter");
-document.body.appendChild(mediumHighlighter);
-
-const setMarkerPosition = (markerPosition) =>
-mediumHighlighter.setAttribute(
-  "markerPosition",
-  JSON.stringify(markerPosition)
-);
-
+const body = document.querySelector("body");
 
 const getHighlightedWord = async (e) => {
     const selectedText = handleSelection();
     
     let message;
 
-    message = (selectedText.length == 0) ? { isWord: false, word: '' } : { isWord: true , word: selectedText }
+
+    if (selectedText.length == 0) message = { isWord: false, word: '' }
+    else message = { isWord: true , word: selectedText }
 
     console.log(message);
-
-    setMarkerPosition(getMarkerPosition());
         
     // sending message to the background
 
@@ -31,43 +23,7 @@ function handleSelection() {
 }
 
 
-const body = document.querySelector("body");
 body.addEventListener("dblclick", getHighlightedWord);
 
 
 console.log("content Script");
-
-
-
-body.addEventListener("click", () => {
-    if (handleSelection().length > 0) {
-      setMarkerPosition(getMarkerPosition());
-    }
-  });
-  
-  body.addEventListener("dblclick", () => {
-    if (handleSelection().length === 0) {
-      setMarkerPosition({ display: "none" });
-    }
-  });
-  
-  
-  function getMarkerPosition() {
-    const rangeBounds = window
-      .getSelection()
-      .getRangeAt(0)
-      .getBoundingClientRect();
-    return {
-      // Substract width of marker button -> 40px / 2 = 20
-      left: rangeBounds.left + rangeBounds.width / 2 - 20,
-      top: rangeBounds.top - 30,
-      display: "flex",
-    };
-  }
-
-
-// setMarkerPosition({
-//   left: 0,
-//   top: 0,
-//   display: "flex",
-// });
